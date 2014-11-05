f382a8741896dd9305421fb9d22a1204edc498a3
Chris Reuter <chris@blit.ca>
2014-11-04 23:15:07 -0500

    Checkpoint.

--- api/recently-updated/findmod.pl
+++ api/recently-updated/findmod.pl
@@ -8,21 +8,33 @@ use warnings;
 use File::Find;
 use File::stat;
 
-use constant MAX_AGE => 2 * 24*60*60;   # 2 days
-use constant MIN_ITEMS => 10;
+use Getopt::Long;
 
-
+# Defaults
+use constant MAX_AGE => 24;
 
 
 {
-  my @newest = findNewest(MAX_AGE, MIN_ITEMS);
-  
+  my $maxAge = MAX_AGE;
+  my $minItems = MIN_ITEMS;
 
+  my @newest = findNewest($maxAge * 3600);
+
+  makeHtml(MAX_AGE, @newest);
 }
 
 
+sub makeHtml {
+  my @newest = @_;
+
+
+
+
+}
 
 sub findNewest {
+  my ($maxAge, $minItems) = @_;
+
   my @dates = ();
 
   for my $path (glob '/home/*/public_html') {
@@ -40,7 +52,18 @@ sub findNewest {
 
   @dates = sort {$a->[1] <=> $b->[1]} @dates;
 
-  return @dates;
+  # Discard all dates older than $maxAge seconds ago
+  my $earliest = time() - $maxAge;
+  my @recent = grep { $_->[1] > $earliest } @dates;
+
+  # ...but if there are less than the minimum, we ignore that and just
+  # take the $minItems newest.
+  # if (scalar @recent < $minItems) {
+  #   @recent = @dates;
+  #   $#recent = $minItems - 1 if scalar @recent > $minItems;
+  # }
+
+  return @recent;
 }
 
 
